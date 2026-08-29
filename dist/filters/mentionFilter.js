9006bf9fd9fd835e1953dcd08f40f4fbfc628ed6
Alexnitro777 <alexnitrohotz777@gmail.com>
2026-08-29 15:50:46 +0300

    chore: clear git cache and apply .gitignore

--- dist/filters/mentionFilter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sanitizeMentions = sanitizeMentions;
-function sanitizeMentions(text) {
-    return text
-        .replace(/@everyone/g, '@\u200beveryone')
-        .replace(/@here/g, '@\u200bhere')
-        .replace(/<@&\d+>/g, '[роль]');
-}
